eb9f4de6d77f5b72cbd0b8f33a420c85a268a693
Pedro Nieto <pedronieto.2005@gmail.com>
2024-04-02 17:59:27 -0300

    feat: add events route
    Since the organizators have to create a event so that the participants can participate, the creation of the /events route was necessary
    
    This commit adds a POST route '/events' that creates a new event and stores it on the SQLite database via Prisma. This commit also provides data validation with zod, SQL query visualization and migration history with Prisma and the server creation with TypeSript and Fastify

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
+import { z } from "zod";
+
+const app = fastify();
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+
+app.post("/events", async (req, reply) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  });
+  const data = createEventSchema.parse(req.body);
+
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      maximumAttendees: data.maximumAttendees,
+      slug: new Date().toISOString(),
+    },
+  });
+
+  return { eventId: event.id };
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("🔥 HTTP Server is running on port 3333!f");
+});
